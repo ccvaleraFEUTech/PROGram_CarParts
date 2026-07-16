9e824211544809bca3c43fbe11b44bde7ac26cde
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-07-16 10:59:21 +0800

    Function to validate update form

--- js/profile.js
+++ js/profile.js
@@ -4,3 +4,26 @@
     2. contact number
     3. current password, new password, confirm new password
 */
+function validateUpdateForm(form) {
+    // email, phone, and region name
+    
+    const emailInput = form.querySelector('input[name="email"]');
+    const phoneInput = form.querySelector('input[name="contact-number"]');
+    const regionInput = form.querySelector('input[name="region-name"]');
+
+    let isValid = true;
+    
+    if (emailInput && !validateEmail(emailInput)) {
+        isValid = false;
+    }
+
+    if (phoneInput && !validatePhoneNumber(phoneInput)) {
+        isValid = false;
+    }
+
+    if (regionInput) {
+        // TODO: Add region validation
+    }
+
+    return isValid;
+}
