a75250dcb6ac4b8574d819f9f7564894419677ae
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-07-16 12:06:42 +0800

    Remove unnecessary comments

--- js/profile.js
+++ js/profile.js
@@ -1,10 +1,3 @@
-/*
-    Things to validate in the profile.js:
-    1. email address?
-    2. contact number
-    3. current password, new password, confirm new password
-*/
-
 import { validatePhoneNumber, formatPhoneNumber, validateConfirmPassword, hideConfirmPasswordError } from './field_validation.js';
 import { validateEmail } from './authentication.js';
 
