b9eaf7257e516ae05cacfbf9f358210364983733
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-07-16 16:14:05 +0800

    Symbol password requirement valdiation

--- js/authentication.js
+++ js/authentication.js
@@ -72,7 +72,8 @@ export function validatePasswordRequirements(password) {
         length: password.length >= 8,
         uppercase: /[A-Z]/.test(password),
         lowercase: /[a-z]/.test(password),
-        number: /[0-9]/.test(password)
+        number: /[0-9]/.test(password),
+        symbol: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)
     };
 
     return requirements;
