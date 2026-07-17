8a25c43ce0d01a7208db4109edc1b55911bcf391
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-07-18 00:00:29 +0800

    Delete email_confirmation_upgrade.sql

--- database/email_confirmation_upgrade.sql
+++
@@ -1,9 +0,0 @@
-USE program_carparts;
-
-ALTER TABLE users ADD COLUMN IF NOT EXISTS email_status VARCHAR(20) NOT NULL DEFAULT 'Pending' AFTER promotions;
-ALTER TABLE users ADD COLUMN IF NOT EXISTS verification_token VARCHAR(64) NULL AFTER email_status;
-ALTER TABLE users ADD COLUMN IF NOT EXISTS verification_expires_at DATETIME NULL AFTER verification_token;
-
-UPDATE users
-SET email_status = 'Confirmed'
-WHERE verification_token IS NULL;
